22f1c701f304918ead73ff2abe627cdd3394aa2e
kawasaki <yomitanmasa3523@gmail.com>
2021-10-02 23:24:58 +0900

    checkbox test

--- src/js/components/admin/itemCreate.js
+++ src/js/components/admin/itemCreate.js
@@ -6,12 +6,17 @@ const ItemCreate = () =>  {
 
   const [data, setData] = useState([])
   const [image, setImage] = useState("")
+  const [isChecked, setIsChecked] = useState(false);
 
   const onSubmit = (data) => {
-
+console.log(data)
     const itemData = new FormData()
     itemData.append("name", data.name)
     itemData.append("detail", data.detail)
+    itemData.append("normal_price", data.normal_price)
+    itemData.append("special_price", data.special_price)
+    itemData.append("stock", data.stock)
+    itemData.append("display_flg", data.display_flg)
     itemData.append("image", fileInput.current.files[0])
 
     axios.post( '/admin/item/store', itemData, {
@@ -30,30 +35,28 @@ const ItemCreate = () =>  {
   };
 
   const handleChange = (e) => {
-      const target = e.target;
-      const name = target.name;
-      const value = target.type === "file" ? target.files[0] : target.value;
+      const t = e.target;
+      const name = t.name;
+      const value = t.type === "file" ? t.files[0] : t.value;
       setData({...data, [name]: value})
   }
+  
+  const handleCheck = (e) => {
+    const t = e.target;
+    const name = t.name;
+    const value = t.checked === true ? 1 : 0;
+    setData({...data, [name]: value})
+    return (e) => {
+      setIsChecked(t.checked);
+    };
+  };
+
 
   const fileInput = React.createRef()
 
   return (
       <>
-          <input 
-            type="text"
-            name="name"
-            // value={data.name}
-            required="required"
-            onChange={handleChange}
-          />
-          <input 
-            type="textarea"
-            name="detail"
-            // value={data.detail}
-            required="required"
-            onChange={handleChange}
-          />
+        <p>
           <input 
             type="file" 
             multiple="multiple"
@@ -62,6 +65,50 @@ const ItemCreate = () =>  {
             onChange={handleChange}
             accept="image/*"
           />
+        </p>
+        <p>
+          <input 
+            type="text"
+            name="name"
+            // value={setData.name}
+            required="required"
+            onChange={handleChange}
+          />
+        </p>
+        <p>
+          <input 
+            type="textarea"
+            name="detail"
+            // value={data.detail}
+            required="required"
+            onChange={handleChange}
+          />
+        </p>
+        <p>
+          <input 
+            type="text"
+            name="normal_price"
+            // value={data.detail}
+            required="required"
+            onChange={handleChange}
+          />
+        </p>
+          <input 
+            type="text"
+            name="special_price"
+            // value={data.detail}
+            required="required"
+            onChange={handleChange}
+          />
+        <p>
+          <label>公開</label>
+          <input 
+            type="checkbox"
+            name="display_flg"
+            {...isChecked ? true : false}
+            onChange={handleCheck}
+          />
+        </p>
 
           <button type="submit" onClick={() => onSubmit(data)} />登録する
       </>
