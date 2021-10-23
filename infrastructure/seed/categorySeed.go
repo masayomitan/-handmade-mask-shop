a25940c958dcbd4d5755f97716ad32a9ba4b89c9
kawasaki <yomitanmasa3523@gmail.com>
2021-10-23 19:58:50 +0900

    seed

---
+++ infrastructure/seed/categorySeed.go
@@ -0,0 +1,31 @@
+package seed
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+	// "handmade_mask_shop/infrastructure/database"
+	"handmade_mask_shop/domain"
+	"fmt"
+	"time"
+	"gorm.io/gorm"
+)
+
+
+func CategorySeed(db *gorm.DB) error {
+
+	var Category domain.Category
+
+	now := time.Now()
+
+	for i, v := range []string{"マスク", "ピアス", "リング"} {
+		Category.ID += 1
+		Category.Name = v
+		Category.Orderby = (i + 1)
+		Category.CreatedAt = now
+		Category.UpdatedAt = now
+		if err := db.Create(&Category).Error; err != nil {
+      fmt.Printf("%+v", err)
+    }
+	}
+	return nil
+
+}
