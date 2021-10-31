c66918d8af4446684511c78dfbaa4d3450e56738
kawasaki <yomitanmasa3523@gmail.com>
2021-10-31 15:26:32 +0900

    route 分け

--- main.go
+++ main.go
@@ -1,37 +1,36 @@
 package main
 
 import (
-		// "github.com/gin-gonic/gin"
+		"github.com/gin-gonic/gin"
 		_ "github.com/go-sql-driver/mysql"
-		// "fmt"
+		"fmt"
 		// "os"
 		// "handmade_mask_shop/infrastructure/database"
 		"handmade_mask_shop/routes"
-		
 )
 
 func main() {
+	fmt.Println()
+	r := gin.Default()
 
 	// db := database.GormConnect()
 	// database.Migrations(db)
 	// database.Seeds(db)
-	r := routes.GetAdminRoutes()
+	r = routes.GetAdminRoutes(r)
+	r = routes.GetRoutes(r)
 
 		files := []string{ 
-			"./templates/top/index.html", "./templates/top/detail.html",
+			"./templates/top/index.html", "./templates/front/item/detail.html",
 			"./templates/admin/dashboard/index.html", "./templates/admin/item/index.html", "./templates/admin/item/detail.html", "./templates/admin/item/create.html",
 			"./templates/admin/user/regist.html", 
 			"./templates/admin/adminUser/regist.html",
-		  "./templates/layout/dafault.html", "./templates/layout/admin_default.html",
+      "./templates/layout/dafault.html", "./templates/layout/admin_default.html",
 			"./templates/admin/element/header.html", "./templates/admin/element/footer.html", "./templates/admin/element/sidebar.html",
 		}
 
 		r.LoadHTMLFiles(files...)
 		r.Static("/src", "./src")
 		r.Static("/public", "./public")
-
-		r.GET("/", routes.Top)
-		r.GET("/detail:id", routes.TopDetail)
 		
 	r.Run(":80")
 };
--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,22 @@
 package routes
 
 import (
-    "net/http"
+    // "net/http"
     "github.com/gin-gonic/gin"
+    "handmade_mask_shop/controller/front"
 )
 
-func Top(c *gin.Context) {
-    c.HTML(http.StatusOK, "top/index.html", gin.H{})
-}
+// func Top(c *gin.Context) {
+//     c.HTML(http.StatusOK, "top/index.html", gin.H{})
+// }
 
-func TopDetail(c *gin.Context){
-	c.HTML(http.StatusOK, "top/detail.html", gin.H{})
+// func TopDetail(c *gin.Context){
+// 	c.HTML(http.StatusOK, "top/detail.html", gin.H{})
+// }
+
+func GetRoutes(r *gin.Engine) *gin.Engine {
+
+	r.GET("/", controller.Top)
+    r.GET("/item/detail/:id", controller.Detail)
+    return r
 }
