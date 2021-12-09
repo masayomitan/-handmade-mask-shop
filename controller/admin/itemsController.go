e61e5dae7b2093474925f5d4922dd21bdad1831c
kawasaki <yomitanmasa3523@gmail.com>
2021-12-09 23:06:16 +0900

    Update itemsController.go

--- controller/admin/itemsController.go
+++ controller/admin/itemsController.go
@@ -2,38 +2,41 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"fmt"
 	"handmade_mask_shop/domain"
 	"handmade_mask_shop/repository"
 	"handmade_mask_shop/service"
+  "fmt"
+	// "github.com/gin-contrib/sessions"
 	"net/http"
 	"os"
-
 	_ "github.com/go-sql-driver/mysql"
-
+	"github.com/gorilla/csrf"
 )
 
 
 func Index(c *gin.Context) {
-	fmt.Println()
-	data := repository.GetAllDisplayItems()
-
-	c.HTML(http.StatusOK, "admin/item/index.html", gin.H{
+	data := repository.GetAllItems()
+	c.HTML(http.StatusOK, "admin/items/index.html", gin.H{
 		"data" : data,
 	})
 }
 
-//create item with files
-func Create(c *gin.Context) {
 
-  c.HTML(http.StatusOK, "admin/item/create.html", gin.H{})
+func Create(c *gin.Context) {
+	csrf := csrf.Token
+	// session := sessions.Default(c)
+	fmt.Println()
+
+  categories := service.GetJsonAllCategories()
+  c.HTML(http.StatusOK, "admin/items/create.html", gin.H{
+		"categories": categories,
+		"CSRFField" :  csrf,
+	})
 }
 
+
 func Store(c *gin.Context) {
-
-  //saving formData
 	var item domain.Item
-
 	err := c.Bind(&item)
   if err != nil {
 		c.String(http.StatusBadRequest, "Request is failed: "+err.Error())
@@ -41,7 +44,6 @@ func Store(c *gin.Context) {
 	}
 
 	itemId := repository.SaveItem(&item)
-
 	file, _ := c.FormFile("image")
 	newFileName := service.RenameFile(file.Filename)
 	imageDir := "./public/images/" 
@@ -53,26 +55,31 @@ func Store(c *gin.Context) {
       "message": "error. could not find dir",
     })
   }
+
 	er := c.SaveUploadedFile(file, filePath)
 	if er != nil {
+		defer 
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
 				"message": "Unable to save the file",
 		})
 		return
   }
   
-	// saving fileData
-	service.ResizeFile(filePath)
+	// service.ResizeFile(filePath)
 	repository.SaveItemImage(newFileName, itemId)
 }
 
-func Detail(c *gin.Context) {
-	id := c.Param("id")
-	fmt.Println(id)
 
-	item := repository.GetDataById(id)
-	fmt.Println(item)
-	c.HTML(http.StatusOK, "admin/item/detail.html", gin.H{
+func ItemDetail(c *gin.Context) {
+	id := c.Param("id")
+	item := repository.GetItemByID(id)
+	c.HTML(http.StatusOK, "admin/items/detail.html", gin.H{
 		"item" : item,
 	})
+}
+
+
+func Category(c *gin.Context) {
+
+	c.HTML(http.StatusOK, "admin/items/category.html", gin.H{})
 }
