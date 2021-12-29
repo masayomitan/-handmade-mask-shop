a8de71927eb0aa486212a6f63766ae6a77566ec8
kawasaki <yomitanmasa3523@gmail.com>
2021-12-29 21:08:46 +0900

    タイプをjsonに変更

--- domain/itemImage.go
+++ domain/itemImage.go
@@ -7,17 +7,17 @@ import (
 )
 
 type  ItemImage struct {
-	ID           uint    `gorm:"primaryKey"`
-	UserID       uint    `gorm:"user_id"`
-	File_name    string  `form:"file_name"`
-	File_path    string  `form:"file_path"`
-	Orderby      int     `form:"orderby"`
+	ID           uint    	`gorm:"primaryKey"`
+	UserID       uint    	`json:"user_id"`
+	File_name    string  	`json:"file_name"`
+	File_path    string  	`json:"file_path"`
+	Orderby      int     	`json:"orderby"`
 
 
-	CreatedAt    time.Time  `form:"created_at" gorm:"NOT NULL"`
-	UpdatedAt    time.Time  `form:"updated_at" gorm:"NOT NULL"`
+	CreatedAt    time.Time  `json:"created_at" gorm:"NOT NULL"`
+	UpdatedAt    time.Time  `json:"updated_at" gorm:"NOT NULL"`
 	DeletedAt    gorm.DeletedAt  `form:"deleted_at" gorm:"default:'null'"`
 
 }
 
-type ItemImages []Item
+type ItemImages []ItemImage
