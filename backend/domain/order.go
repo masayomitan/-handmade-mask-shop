e4285afb12c37c179993642dbba4ee04a5ac040a
kawasaki <yomitanmasa3523@gmail.com>
2022-04-27 20:44:37 +0900

    next auth

--- backend/domain/order.go
+++ backend/domain/order.go
@@ -7,12 +7,12 @@ import (
 )
 
 type Order struct {
-	ID              uint     `gorm:"primaryKey"`
-	UserID          uint   `gorm:"user_id"`
-	CartID          uint   `gorm:"cart_id"`
+	ID               uint    `gorm:"primaryKey"`
+	UserID           uint    `gorm:"user_id"`
+	CartID           uint    `gorm:"cart_id"`
 
 	PaymentID        uint    `json:"payment_id"`
-	Pre_orderID       string  `json:"pre_order_id"`
+	Pre_orderID      string  `json:"pre_order_id"`
 	Message          string  `json:"message"`
 	Family_name      string  `json:"family_name"`
 	First_name       string  `json:"first_name"`
@@ -24,12 +24,12 @@ type Order struct {
 	Birthday         time.Time `json:"birthday"`
   SexID            uint      `json:"sex_id"`
 
-	PrefID           uint      `json:"pref_id"`
-	Postal_code      int       `json:"postal_code"`
-	Address_1        string    `json:"address_1"`
-	Address_2        string    `json:"address_2"`
+	PrefID           uint      	 `json:"pref_id"`
+	Postal_code      int       	 `json:"postal_code"`
+	Address_1        string    	 `json:"address_1"`
+	Address_2        string    	 `json:"address_2"`
 
-	Sub_total     decimal.Decimal `json:"sub_total"`
+	Sub_total    decimal.Decimal `json:"sub_total"`
 	Discount     decimal.Decimal `json:"discount"`
 	Delivery_fee decimal.Decimal `json:"delivery_fee"`
 	Tax          decimal.Decimal `json:"tax"`
@@ -38,10 +38,10 @@ type Order struct {
 	Total_Quantity int 						 `json:"total_quantity"`
 	Currency_code  string  				 `json:"currency_code"`
 
-	Add_point      int   `json:"add_point"`
-  Use_point      int   `json:"use_point"`
+	Add_point      int      `json:"add_point"`
+  Use_point      int      `json:"use_point"`
 	
-	Order_date   time.Time  `form:"order_date" gorm:"NOT NULL"`
+	Order_date   time.Time  `json:"order_date"`
 	
 	CreatedAt    time.Time  `form:"created_at" gorm:"NOT NULL"`
 	UpdatedAt    time.Time  `form:"updated_at" gorm:"NOT NULL"`
